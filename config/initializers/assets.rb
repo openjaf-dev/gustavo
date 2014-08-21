7a3e468956a95a29546c6595e547c1abab4b14f3
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-08-21 01:45:16 -0400

    setting a rails way the location show view

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,3 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( spree/admin/google.js )
-Rails.application.config.assets.precompile += %w( spree/admin/google_show.js )
-
