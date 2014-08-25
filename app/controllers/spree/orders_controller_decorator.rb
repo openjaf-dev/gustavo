5d9060de8741d027f9dfd36e8388776daeba0737
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-08-25 13:49:04 -0400

    no Cart step, the checkout starts when click the add to cart bottom, Delivery Step removed

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -16,13 +16,32 @@ Spree::OrdersController.class_eval do
       populator = Spree::OrderPopulator.new(current_order(create_order_if_necessary: true), current_currency)
       if populator.populate(product.master.id, params[:quantity])
         current_order.ensure_updated_shipments
-
-        respond_with(@order) do |format|
-          format.html { redirect_to cart_path }
-        end
+        # respond_with(@order) do |format|
+        @order = current_order
+        self.update
+          # format.html { redirect_to cart_path }
+        # end
       else
         flash[:error] = populator.errors.full_messages.join(" ")
         redirect_to :back
+      end
+    end
+
+    def update
+      params[:checkout] = true
+      if @order.contents.update_cart(order_params)
+        respond_with(@order) do |format|
+          format.html do
+            if params.has_key?(:checkout)
+              @order.next if @order.cart?
+              redirect_to checkout_state_path(@order.checkout_steps.first)
+            else
+              redirect_to cart_path
+            end
+          end
+        end
+      else
+        respond_with(@order)
       end
     end
 
