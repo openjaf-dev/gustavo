7b2a5c6b72cb442c0ef20280fa59b9cd9609e1b8
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-09-15 16:14:27 -0400

    the room counter

--- vendor/assets/javascripts/spree/frontend/seach_box.js
+++ vendor/assets/javascripts/spree/frontend/seach_box.js
@@ -1,7 +1,8 @@
+var count = 2;
+
 $(document).ready(function () {
 
     $('#add').on("click", function () {
-          var count = 2;
           var newDiv=document.createElement('fieldset');
           var html = '<legend>';
           html += "Room "+count ;
@@ -12,18 +13,23 @@ $(document).ready(function () {
           <select name='roomGroup[room"+count+"][numberOfChildren]' id='roomGroup_room"+count+"_numberOfChildren'><option value='0'>0</option><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select>";
           newDiv.innerHTML= html;
           document.getElementById('select-container').appendChild(newDiv);
+          count++;
       return false;
     });
 
-     $('#remove').on("click", function () {
+    $('#remove').on("click", function () {
           var newDiv=document.createElement('fieldset');
           var html = '<legend>';
           html += "room";
 
           newDiv.innerHTML= html;
           document.getElementById('select-container').appendChild(newDiv);
-
+          count--;
       return false;
+    });
+
+    $("#roomGroup_room"+count+"_numberOfChildren").on('change', function() {
+        alert( this.value ); // or $(this).val()
     });
 
     var input = document.getElementById('location_search');
