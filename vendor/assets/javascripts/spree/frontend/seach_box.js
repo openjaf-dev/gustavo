4b1e3df800099df990349592ed34290ec17d63af
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-09-15 22:32:33 -0400

    jave script customization

--- vendor/assets/javascripts/spree/frontend/seach_box.js
+++ vendor/assets/javascripts/spree/frontend/seach_box.js
@@ -3,7 +3,7 @@ var count = 2;
 $(document).ready(function () {
 
     $('#add').on("click", function () {
-          var newDiv=document.createElement('fieldset');
+          var newFieldset=document.createElement('fieldset');
           var html = '<legend>';
           html += "Room "+count ;
 
@@ -11,8 +11,8 @@ $(document).ready(function () {
           <select name='roomGroup[room"+count+"][numberOfAdults]' id='roomGroup_room"+count+"_numberOfAdults'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select> \
           <label for='Children'>Children</label> \
           <select name='roomGroup[room"+count+"][numberOfChildren]' id='roomGroup_room"+count+"_numberOfChildren'><option value='0'>0</option><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select>";
-          newDiv.innerHTML= html;
-          document.getElementById('select-container').appendChild(newDiv);
+          newFieldset.innerHTML= html;
+          document.getElementById('select-container').appendChild(newFieldset);
           count++;
       return false;
     });
@@ -28,9 +28,22 @@ $(document).ready(function () {
       return false;
     });
 
-    $("#roomGroup_room"+count+"_numberOfChildren").on('change', function() {
-        alert( this.value ); // or $(this).val()
+    $("#roomGroup_room1_numberOfChildren").change(function() {
+        var newDiv=document.createElement('div');
+        var html = "<label for='Adults'>Age of each Children</label>"
+        for(var i = 1; i <= $('#roomGroup_room1_numberOfChildren :selected').val(); i++) {
+            html += "<select name='age' id='test'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select>"
+        }
+        newDiv.innerHTML= html;
+        document.getElementById('room1').appendChild(newDiv)
+
     });
+
+    $('#my_select').change(function() {
+        	   // assign the value to a variable, so you can test to see if it is working
+        	    var selectVal = $('#my_select :selected').val();
+        	    alert(selectVal);
+        	});
 
     var input = document.getElementById('location_search');
     var autocomplete = new google.maps.places.Autocomplete(input);
