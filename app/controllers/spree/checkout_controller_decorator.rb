0eea5f375e17341f51f001fa3b9e8941f3258c5f
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-09-18 17:14:05 -0400

    generating pax field from room quantity, but not working at all jet, i am tired

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -55,13 +55,22 @@ module Spree
 
 	def before_pax
 	  # TODO aqui se da el problema de que la cantidad de pax se suman por el tema de la session que no aspira
-	  if @order.pax_contacts.empty?
-	    sum = 0
-	    @order.line_items.each do |li|
-	      sum += li.quantity
-	    end
-	    sum.times { @order.pax_contacts.new }
-	  end
+	  context = eval(@order.context.context)
+    counter = 0
+    context.each do |key, value|
+       if key == key.scan(/room[0-9]*$/).first
+         counter += 1
+       end
+    end
+    counter.times { @order.pax_contacts.new }
+
+    #if @order.pax_contacts.empty?
+	  #  sum = 0
+	  #  @order.line_items.each do |li|
+	  #    sum += li.quantity
+	  #  end
+	  #  sum.times { @order.pax_contacts.new }
+	  #end
 	end
 
   end
