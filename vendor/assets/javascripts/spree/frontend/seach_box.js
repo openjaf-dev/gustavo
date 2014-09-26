3e393d43b3d466e2c79a971d66c060f84a723082
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-09-25 22:15:48 -0400

    some fixing to senitize params

--- vendor/assets/javascripts/spree/frontend/seach_box.js
+++ vendor/assets/javascripts/spree/frontend/seach_box.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     $('#add').on("click", function () {
         var newFieldset=document.createElement('fieldset');
-        newFieldset.id = "room"+count+""
+        newFieldset.id = "room_"+count;
         var html = '<legend>';
         html += "Room "+count ;
 
@@ -12,28 +12,29 @@ $(document).ready(function () {
         <select name='roomGroup[room"+count+"][numberOfAdults]' id='roomGroup_room"+count+"_numberOfAdults'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select> \
         <label for='Children'>Children</label> \
         <select name='roomGroup[room"+count+"][numberOfChildren]' id='children_"+count+"' class='children_select'><option value='0'>0</option><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select>\
-        <a href='#' id='remove"+count+"'>X</a>        ";
+        <a href='#' id='remove_"+count+"'>X</a>        ";
         newFieldset.innerHTML= html;
         document.getElementById('select-container').appendChild(newFieldset);
         $("#children_" + count).change(children_changed);
+        $("#remove_" + count).click(remove_room);
         count++;
       return false;
     });
 
-//    $("#remove"+count--+"").on("click", function () {
-//        $("#room"+count--+"").remove();
-//        count--;
-//      return false;
-//    });
-//    a todos los select que esten dentro de un fildset
-//    combo1.chan
-//
-//    todos los combos se suscriben a la misma funcion.
+    function give_the_number(id) {
+        var parts = id.split("_");
+        var number = parts[parts.length - 1];
+        return number;
+    }
+
+    function remove_room(event) {
+        var number = give_the_number(event.target.id);
+        $("#room_" + number).remove();
+        //count --;
+    }
 
     function children_changed(event) {
-        console.log(event);
-        var parts = event.target.id.split("_");
-        var number = parts[parts.length - 1];
+        var number = give_the_number(event.target.id);
         var new_div_id = "room_" + number + "_children";
         $("#" + new_div_id).remove();
         var number_selected = $("#" + event.target.id + ' :selected').val();
@@ -46,7 +47,7 @@ $(document).ready(function () {
                 html += "<select name='roomGroup[room" + number + "][age"+i+"]' id='test'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select>"
             }
             newDiv.innerHTML= html;
-            document.getElementById('room'+number).appendChild(newDiv);
+            document.getElementById('room_'+number).appendChild(newDiv);
         }
     }
 
