8a9873d123d0074e3879170aba748f76cb790f44
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-09-26 22:55:28 -0400

    made a helper method to simulate the hotel_path

--- app/helpers/spree/hotels_helper.rb
+++ app/helpers/spree/hotels_helper.rb
@@ -1,2 +1,14 @@
 module Spree::HotelsHelper
+
+  def hotels_path(id, params)
+    link = "hotels/#{id}"
+    if params
+      link += "?"
+      params.each do |key, value|
+        link += "#{key}=#{value}&"
+      end
+    end
+    link
+  end
+
 end
