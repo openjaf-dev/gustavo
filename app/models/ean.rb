586f0de2551ae8fee1598f27721e40b4a6d66685
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-09-26 22:56:02 -0400

    more generalized

--- app/models/ean.rb
+++ app/models/ean.rb
@@ -1,5 +1,24 @@
 class Ean
 
+  def self.room_group_convert(hash, params)
+
+    #TODO hecho con 'each_with_index' hasta que se solucione la consecutidad de las habitaciones
+    params[:roomGroup].each_with_index do |room, i|
+
+      ages = ""
+      if room[1][:numberOfChildren].to_i > 0
+        for j in 1..room[1][:numberOfChildren].to_i
+          ages += room[1]["age#{j}"]
+          ages += "," if j != room[1][:numberOfChildren].to_i
+
+        end
+        hash["room"+(i+1).to_s] = "#{room[1][:numberOfAdults]}"+","+ages
+      else
+        hash["room"+(i+1).to_s] = room[1][:numberOfAdults]
+      end
+    end
+  end
+
   def self.ean_to_params
 
   end
@@ -10,23 +29,9 @@ class Ean
     ean_params[:arrivalDate] = params[:arrivalDate]
     ean_params[:departureDate ] = params[:departureDate]
 
-    #TODO hecho con 'each_with_index' hasta que se solucione la consecutidad de las habitaciones
-      params[:roomGroup].each_with_index do |room, i|
+    room_group_convert(ean_params, params)
 
-      ages = ""
-      if room[1][:numberOfChildren].to_i > 0
-        for j in 1..room[1][:numberOfChildren].to_i
-          ages += room[1]["age#{j}"]
-          ages += "," if j != room[1][:numberOfChildren].to_i
-
-        end
-        ean_params["room"+(i+1).to_s] = "#{room[1][:numberOfAdults]}"+","+ages
-      else
-        ean_params["room"+(i+1).to_s] = room[1][:numberOfAdults]
-      end
-    end
-
-    asasasdas
+    #TODO duda si es mejor hacerlo con Lat y Long y no con el location_search
     if (params[:location_lat] != "") && (params[:location_lng] != "")
 
       ean_params[:latitude] = params[:location_lat]
