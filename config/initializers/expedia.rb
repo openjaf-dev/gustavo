9223c9a19c6fb4e0d95ea97ba438f5cd4f762118
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-10-01 15:21:01 -0400

    using figaro

---
+++ config/initializers/expedia.rb
@@ -0,0 +1,10 @@
+Expedia.setup do |config|
+    config.cid = 55505 # your cid once you go live.
+    config.api_key = ENV['expedia_api_key']
+    config.shared_secret = ENV['expedia_shared_secret']
+    config.locale = 'en_US' # For Example 'de_DE'. Default is 'en_US'
+    config.currency_code = 'USD' # For Example 'EUR'. Default is 'USD'
+    config.minor_rev = 26 # between 4-26 as of July 2014. Default is 4. 26 being latest
+end
+
+$api = Expedia::Api.new
